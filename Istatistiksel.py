d0cbc3cbe966f6de5ce41330ee95bb200dd9002d
Furkan Akkaya <furkanakkaya245@gmail.com>
2026-01-22 17:23:17 +0300

    ıstatistik_son

--- Istatistiksel.py
+++ Istatistiksel.py
@@ -37,23 +37,22 @@ scale=10
 
 loc=1
 scale= 4**0.5
-print(f"1-a: {norm.cdf(2.4,loc,scale)}")
+print(f"1-a: {norm.cdf(2.4,loc,scale)}") # Direkt Problem
 print(f"1-b: {norm.cdf(-1.1,loc,scale)}")
 print(f"1-c: {1-norm.cdf(1.1,loc,scale)}")
 print(f"1-d: {norm.cdf(8,loc,scale)-norm.cdf(2,loc,scale)}\n")
-
 loc=0
 scale= 1**0.5
-print(f"2-a: {norm.ppf(0.10,loc,scale)}")
+print(f"2-a: {norm.ppf(0.10,loc,scale)}") # Ters Problem
 print(f"2-b: {norm.ppf(0.20,loc,scale)}")
 print(f"2-c: {norm.ppf((1-0.05),loc,scale)}")
-print(f"2-d: {norm.ppf(0.975,loc,scale)}\n") # 0.95 in ortada olması için
-
+print(f"2-d_min: {norm.ppf(0.025,loc,scale)}")
+print(f"2-d_max: {norm.ppf(0.975,loc,scale)}\n") # -c ve +c arası 0.95 olduğu için simetrik olması için 0.025sol tarafı alındı. 
+# ortalama 0 dan farlı olmasından kaynaklı böyle 0 olmasaydı 0.025 ve 0.975 alınacaktı.iki taraflı ölçü alınacaktı
 loc=1
 scale=0.01**0.5
 print(f"3-a: {1-(norm.cdf(1.02,loc,scale)-norm.cdf(0.98,loc,scale))}")
 ust_sinir = norm.ppf(0.995, loc, scale)
 alt_sinir = norm.ppf(0.005, loc, scale)
 print(f"3-b: [{alt_sinir:.4f} mm, {ust_sinir:.4f} mm]")
-
 
